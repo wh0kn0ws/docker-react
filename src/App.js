68a55e1e304bb3b7f693c0b8602804116213bd52
Anis Messaoudi <anis.messaoudi@se.univ-bejaia.dz>
2022-01-31 06:31:37 -0700

    modified index text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         It works. yes!
+         Modified by Beta
         </p>
         <a
           className="App-link"
